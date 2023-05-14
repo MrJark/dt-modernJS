95fadf871a47e4f95e17fefcb34a3eea5569e4a7
Chema <mrjark7@gmail.com>
2023-05-14 12:31:13 +0200

    t

---
+++ 01-fundamentals/app.js
@@ -0,0 +1,8 @@
+
+let a = 10,
+    b = 20,
+    c = 30,
+    d = 40;
+
+console.log( {a, b, c} ); // los corchetes permiten mostrar el valor y la key a la vez
+console.table({a,b,c});// da los valores en formato tabla pero son necesarios los corchetes

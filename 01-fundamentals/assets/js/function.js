3dc42d168da0a788fe7f015f5a38f729e7661aae
Chema <mrjark7@gmail.com>
2023-05-15 10:21:33 +0200

    've added differents method to call functions

--- 01-fundamentals/assets/js/function.js
+++ 01-fundamentals/assets/js/function.js
@@ -11,4 +11,30 @@ const salu2 = function () {
 
 const u_U = () => { // arrow function y son las más utilizadas
 
-}
+};
+
+// Retos
+
+function sumar (a, b) {
+    return a +' '+ b;
+};
+console.log(sumar('Hola', 'Mundo'));
+
+function sumar2 (a, b) {
+    return a + b;
+};
+console.log(sumar2(2,3));
+// la siguiente función hace lo mismo que la de sumar anterior pero más corta ya que solo quiero el return, en el caso que tuviera más elementos, no se podría hacer
+
+const sumar3 = (a,b) => a + b; 
+console.log(sumar3(2,7));
+
+// Reto: transformar getAleatorio en getAleatorio2 pero siendo arrrow function
+function getAleatorio() {
+    return Math.random()*100;
+};
+console.log('Random1:',getAleatorio());
+
+const getAleatorio2 = () => Math.random()*100; 
+console.log('Random2:',getAleatorio2());
+

200e70e3e542c54fe44944d2c840329d925227e1
Chema <mrjark7@gmail.com>
2023-05-15 09:56:23 +0200

    differents type of functions

---
+++ 01-fundamentals/assets/js/function.js
@@ -0,0 +1,14 @@
+
+// Formas para definir funciones
+
+function saludar() { // lo que se coloca dentro de los paréntesis son los argumentos
+    console.log('Hola mundo');
+};
+
+const salu2 = function () {
+    console.log('Hola Mundo');
+};
+
+const u_U = () => { // arrow function y son las más utilizadas
+
+}

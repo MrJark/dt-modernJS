e6b04ba31e13f4bcfe4cb0d327cc41d288e678a6
MrJark <mrjark7@gmail.com>
2023-05-21 09:06:49 +0200

    added get and set

--- 01-fundamentals/assets/js/classes/class.js
+++ 01-fundamentals/assets/js/classes/class.js
@@ -7,12 +7,22 @@ class Persona {
     nombre;
     codigo;
     frase;
+    comida;
 
     constructor( nombre, codigo, frase ) {
         // this. hace referencia al objeto, en este caso a la clase Persona
         this.nombre = nombre;
         this.codigo = codigo;
         this.frase = frase;
+    }
+
+    set comidaFavorita( comida ) { // el set solo recibe un argumento y no puede tener el mismo nombre que la propiedad a la que llama
+        // los sets son para establecer valores 
+        this.comida = comida;
+    }
+
+    get comidaFavoritaDar () {
+        return `La comida fav de ${this.nombre} es ${this.comida}`;
     }
 
     // creación de métodos y se hacen debajo del constructor
@@ -29,3 +39,7 @@ const ironman = new Persona('Tony Stark', 'Iron Man', 'Yo soy Iron Man');
 
 ironman.quienSoy();
 ironman.miFrase();
+ironman.comidaFavoritaDar;
+
+ironman.comidaFavorita = 'Shawarma'; // aquí se establece el valor del set
+console.log(ironman);

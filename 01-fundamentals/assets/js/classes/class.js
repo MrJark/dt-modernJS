6b85093a374075eca9856d914164898d20d4b88b
MrJark <mrjark7@gmail.com>
2023-05-21 08:17:57 +0200

    class initialisation

--- 01-fundamentals/assets/js/classes/class.js
+++ 01-fundamentals/assets/js/classes/class.js
@@ -1,13 +1,20 @@
 
 class Persona {
 
+    // estas son propiedades que le añado a mi clase persona, pueden ser cualquiera propiedad ya que es para saber que va a tener 'dentro' la clases.
+    // buena práctica ponerlas al inicio para saber que va a contener
+    // las clases tienen el 'use strtict' por defecto
     nombre;
     codigo;
     frase;
 
-    constructor() {
-        console.log('Hola');
+    constructor( nombre, codigo, frase ) {
+        // this. hace referencia al objeto, en este caso a la clase Persona
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.frase = frase;
     }
 }
 
-const spiderman = new Persona();
+// a esta new Persona tengo que añadirle entre paréntesis tanto el nombre, como el código como la frase. Todo entre '' y separados por comas y EN EL ORDEN INDICADO
+const spiderman = new Persona(); 

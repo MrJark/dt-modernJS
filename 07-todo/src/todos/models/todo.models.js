185cc5803b33bae391c0b61acf30e38f6be375e1
Chema <mrjark7@gmail.com>
2023-05-27 14:35:39 +0200

    its doesn't work, this is the message:https://www.google.com/search?q=get+http%3A%2F%2Flocalhost%3A5173%2Fnode_modules%2F.vite%2Fdeps%2Fuuid.js%3Fv%3D616f4974+net%3A%3Aerr_aborted+504+(outdated+optimize+dep)&oq=&aqs=chrome.0.35i39i362l7j69i59i450.3484484j0j7&sourceid=chrome&ie=UTF-8

--- 07-todo/src/todos/models/todo.models.js
+++ 07-todo/src/todos/models/todo.models.js
@@ -1,8 +1,15 @@
+// esto genera id únicos para cada una de las tareas que vas a tener 
+
+import { v4 as uuidv4 } from 'uuid';
 
 export class Todo {
     
+    /**
+     * 
+     * @param {string} description 
+     */
     constructor( description ) {
-        this.id = 1;
+        this.id = uuidv4();
         this.description = description;
         this.done = false;
         this.createAt = new Date();

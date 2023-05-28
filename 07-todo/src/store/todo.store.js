bc25d923fe59aa4c053a5221209e67a3c8245f1f
MrJark <mrjark7@gmail.com>
2023-05-28 08:06:49 +0200

    've added the necesaries functions to do the todo app in todo.store.js

--- 07-todo/src/store/todo.store.js
+++ 07-todo/src/store/todo.store.js
@@ -6,6 +6,7 @@ const Filters = {
     Pending: 'Pending',
 }
 
+
 const state  = {
     todos: [
         new Todo('PIEDRA'),
@@ -17,11 +18,103 @@ const state  = {
     filter: Filters.All,
 }
 
+// Funciones que necesitas para crear la aplicación de TODOS
+
 const initStore = () => {
     console.log(state);
     console.log('InitStore🍍');
-}
+};
+
+const loadStore = () => {
+    throw new Error('No implemented yet');
+};
+
+/**
+ * Muestra los todos dependiendo del filtro, sino los enseña todos
+ * @param {string} filter 
+ * Puede tener 4 estados: all, completed, pending o no valid
+ */
+const getTodos = ( filter = Filters.All ) => {
+    // throw new Error('No implemented yet');
+
+    // Hay 3 panoramas en esta función ya que existen 3 filtros: all, completed y pending
+    switch ( filter ) {
+        case Filters.All:
+            return [...state.todos];
+        case Filters.Completed:
+            return state.todos.filter( todo => todo.done ); 
+            // El filter permite saber si todo está acabado, se podria poner todo => todo.done == true pero sería redundante ya que el .done me dice que está acabado
+        case Filters.Pending:
+            return state.todos.filter( todo => !todo.done );
+            // La negación del done es pendiente
+        default:
+            throw new Error(`El filtro ${ filter } no es valido`);
+    };
+};
+
+/**
+ * Añade un todo a través de la description con su respectivo id
+ * @param {string} description 
+ */
+const addTodo = ( description ) => {
+    // throw new Error('No implemented yet');
+    if ( !description ) throw new Error('Description is required');
+    state.todos.push( new Todo(description));
+};
+
+/**
+ * 
+ * @param {string} todoId : Todo identify
+ */
+const toggleTodo = (todoId) => {
+    throw new Error('No implemented yet');
+};
+
+/**
+ * Elimina un todo gracias a su id
+ * @param {string} todoId : Todo identify
+ */
+const deleteTodo = (todoId) => {
+    // throw new Error('No implemented yet');
+    state.todos = state.todos.filter( todo => todo.id != todoId); 
+    // Devulevo todos los todos que tengan el id distinto al cual ha borrado
+};
+
+/**
+ * Asigna 'completed' a un todo gracias a su id
+ * @param {string} todoId : Todo identify
+ */
+const deleteCompleted = (todoId) => {
+    // throw new Error('No implemented yet');
+    state.todos = state.todos.filter( todo => todo.done ); 
+    // Parecido al delete pero tiene que aquellos todos que no tengan la propiedad done
+};
+
+/**
+ * Para mostrar solo aquellos TODO que estén dentro del filtro 
+ * @param {Filters} newFilter 
+ */
+const setFilter = ( newFilter = Filters.All ) => {
+    // throw new Error('No implemented yet');
+    state.filter = newFilter;
+};
+
+/**
+ * Para limitar la capacidad de los Usuarios de manipular el store
+ */
+const getCurrentFilter = () => {
+    // throw new Error('No implemented yet');
+    return state.filter;
+};
 
 export default {
+    addTodo,
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
+    getTodos,
     initStore, 
+    loadStore,
+    setFilter,
+    toggleTodo,
 }

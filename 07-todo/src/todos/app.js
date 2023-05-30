6626dc675fc603b6296ce4991d83d52f89cca348
Chema <mrjark7@gmail.com>
2023-05-30 11:46:02 +0200

    error solve: the error was the ' k ' in keyCode. I had put the 'k' in capitals

--- 07-todo/src/todos/app.js
+++ 07-todo/src/todos/app.js
@@ -1,7 +1,7 @@
 // da error si no pones ' ?raw ' porque está esperando un módulo de js y no un html. Lo bueno de vite es que poniendo ?raw se soluciona
-import { renderTodos } from './use-cases';
 import html from './app.html?raw'
 import todoStore from '../store/todo.store';
+import { renderTodos } from './use-cases';
 
 // Función para llamra los distintos ids y classes del app.html y que no hayan strings porque es muy volátil. Si en cambian los ids o classses y los tenes en muchas funciones, puede fallar la app si no los quitas, sin embargo, si los tienes todos en una misma fucnión, solo tienes que cambiarlos cada uno, una sola vez
 const ElementsIds = {
@@ -38,16 +38,16 @@ export const App = ( elementId ) => {
     // Listeners
 
     newDescriptionInput.addEventListener('keyup', ( event ) => {
-        if ( event.KeyCode !== 13 ) return;
+        console.log(event);
+        if ( event.keyCode !== 13 ) return;
         // Los KeyCode son los códigos únicos de cada una de las teclas del teclado, el 13 es la de la tecla " enter " por tanto, cuando se pulse, se habrá creado una una descripción de tarea
         
         if ( event.target.value.trim().length === 0) return; 
         // .trim() elimina todos los espacios vacios por tanto, esto queire decir que si el evento, quitando todos los espacios es 0, es decir, o se ha escrito nada, no se haga nada porque no queremos tareas vacias
         todoStore.addTodo( event.target.value );
-
+        event.target.value = ''; // para limpiar la caja de eventos cuando lo hemos mandado
         displayTodos();
 
-        event.target.value = ''; // para limpiar la caja de eventos cuando lo hemos mandado
     });
 
 }

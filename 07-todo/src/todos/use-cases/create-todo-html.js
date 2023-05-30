aafce942ea68bf6efde36f95665aabc480253755
MrJark <mrjark7@gmail.com>
2023-05-30 06:46:24 +0200

    added todo elements in crate-todo.html

--- 07-todo/src/todos/use-cases/create-todo-html.js
+++ 07-todo/src/todos/use-cases/create-todo-html.js
@@ -7,10 +7,36 @@ import {Todo} from "../models/todo.models";
 export const createTodoHTML = ( todo ) => {
     if ( !todo ) throw new Error('A TODO object is required');
 
-    const html = `<h1>${todo.description}</h1>`;
+    /*
+    copiamos el código de app.html porque es "poco", sino tendríamos que contruirlo de otra manera
+    la clase completed solo aparecerá si el todo está en done y el data-id será un atributo que corresponderá
+    a cada id identificativo
+    */
 
+    // para no escribir siempre " todo. " y lo que queramos del mismo, podemos hacer una deconstrucción del todo y llamar lo que nos haga falta y así evitarnos código
+    const { done, description, id} = todo; 
+    
+    const html = `
+        
+        <div class="view">
+            <input class="toggle" type="checkbox" ${ done ? 'checked' : '' }>
+            <label>${ description }</label>
+            <button class="destroy"></button>
+        </div>
+        <input class="edit" value="Create a TodoMVC template">
+        
+    `;
+
+    /*
+    como estamos crando el 'li' con js, el li del html 
+    <li class="${ todo.done ? 'checked' : '' }" data-id="abc"></li>
+    sobra pero las clases y los ids necesitamos crearlos
+    */
     const liElement = document.createElement('li');
     liElement.innerHTML = html;
+    liElement.getAttribute('data-id', id); // le damos la clase data-id para identificarlo
+    todo.done ? liElement.classList.add('completed') : '';
+    // liElement.classList.add('completed'); // solo si todo.done == true
 
     return liElement;
 };
--- 07-todo/src/todos/use-cases/render-todo.js
+++ 07-todo/src/todos/use-cases/render-todo.js
@@ -1,6 +1,8 @@
 import { createTodoHTML } from "./create-todo-html";
 import { Todo } from "../models/todo.models"
 
+let element;
+
 /**
  * 
  * @param {string} elementId 
@@ -10,7 +12,19 @@ export const renderTodos = ( elementId, todos = [] ) => {
     // Esta función no regresa nada ya que es en esta función donde se van a crear los todos
     // console.log(elementId, todos);
 
-    const element = document.querySelector( elementId );
+    // const element = document.querySelector( elementId );
+    /*
+    no es eficiente tener esta línea ↑ pero es algo visual que te permite ver cómo funciona js. Para hacerla más eficiente hacemos: inicializar una variable en el scope general para llamarla dentro de esta función. Lo eficiente es lo siguiente:
+    */ 
+    // !element ? document.querySelector( elementId ) : element;
+    // !element ? throw new Error(`Element ${ elementId } is not found`) : '';
+    // No he conseguido hacerlo de esta manera porque tenía !element iguales y le asignaba valores distintos
+
+    if ( !element )
+        element = document.querySelector( elementId );
+    if ( !element ) throw new Error(`Your id, ${elementId}, is not found`)
+
+    element.innerHTML = ''; // para que no apile y acumule los distintos todos. Para purgar
 
     todos.forEach( todo => {
         // element.append(createTodoHTML( todo ) );

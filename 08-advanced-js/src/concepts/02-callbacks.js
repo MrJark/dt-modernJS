ee09611b791fdc607b10adcf793498ca580ea2db
Chema <mrjark7@gmail.com>
2023-06-01 13:25:14 +0200

    've added the way callbacks errors used to be solved before

--- 08-advanced-js/src/concepts/02-callbacks.js
+++ 08-advanced-js/src/concepts/02-callbacks.js
@@ -1,12 +1,39 @@
+import { heroes } from '../data/heroes';
 
 /**
  * 
  * @param {HTMLDListElement} element 
+ * @param { (hero: Object) => void } callback
  */
 export const callbacksComponent = ( element ) => {
-
     console.log('callbacks');
 
+    const id = '5d86371f1efebc31def272e2';
+    findHero(id, (error, hero) => {
 
+        if (error) {
+            element.innerHTML = error;
+            return;
+        }
+        element.innerHTML = hero.name;
+    });
 
+};
+
+/**
+ * Ejemplo de manejo de errores con callbacks (estándares antiguos)
+ * @param {String} id 
+ * @param {(error: String|Null, hero: Object) => void} callback 
+ */
+const findHero = ( id, callback ) => {
+    
+    const hero = heroes.find ( hero => hero.id === id );
+
+    if (!hero) {
+        callback (`Error with your id: ${id}`);
+        return; // undefined
+    };
+    
+
+    callback( null, hero ); // esto es el callback
 };
